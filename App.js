8d804fdb0c5b3a507dceb90c99e4d9239e49e96e
varun mohan <varunmohan@varuns-MacBook-Air.local>
2020-08-24 23:29:11 +0530

    tobechanged

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {createSwitchNavigator, createAppContainer} from'react-navigation';
+import QuestionScreen from './screens/QuestionScreen';
+import HomeScreen from './screens/HomeScreen';
+import ResultScreen from './screens/ResultScreen';
+
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style = {styles.container}>
+       <AppContainer/>
+       
+      </View>
+    )
+  }
 }
+
+const switchNavigator = createSwitchNavigator({
+  HomeScreen: {
+    screen: HomeScreen
+  },
+  QuestionScreen: {
+    screen: QuestionScreen
+  },
+
+  ResultScreen:{
+    screen :ResultScreen
+  }
+
+ 
+  
+})
+
+
+const AppContainer = createAppContainer(switchNavigator)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00b5cc',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
